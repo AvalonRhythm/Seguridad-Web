56bc11ae2dd68f254a4fd438a56096ce8fda118c
avasilica001 <andreeavasilica188@gmail.com>
2022-10-15 20:37:30 +0200

    commit prueba

--- app/php/BDConnection.php
+++ app/php/BDConnection.php
@@ -1,10 +1,8 @@
 <?php
-
     #Nos conectamos a la base de datos
     $conexion = mysqli_connect("db", "admin", "test", "database");
 
     if($conexion == false){
         die("ERROR EN LA CONEXIÓN" . mysqli_connect_error());
     }   
-
 ?>
