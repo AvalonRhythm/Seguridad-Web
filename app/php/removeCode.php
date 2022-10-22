a5239d231d711716b73b45697276db128e1419b2
avasilica001 <andreeavasilica188@gmail.com>
2022-10-22 17:49:49 +0200

    ya elimina

--- app/php/removeCode.php
+++ app/php/removeCode.php
@@ -1,30 +1,25 @@
 <?php
-    require_once("php/BDConnection.php");
-    // Connect to database
-
-    extract($_POST);
+require_once("php/BDConnection.php");
     // Get all the categories from category table
     $sql = "SELECT * FROM animales";
     $all_categories = mysqli_query($conexion,$sql);
-
+  
     // The following code checks if the submit button is clicked
     // and inserts the data in the database accordingly
     if(isset($_POST['adoptar']))
     {
-        // Store the Product name in a "name" variable
-        echo "sale algo?";
         echo $_POST['Animal'];
         // Store the Category ID in a "id" variable
-        $id = mysqli_real_escape_string($conexion,$_POST['Animal']);
-
+        $animal = mysqli_real_escape_string($conexion,$_POST['Animal']);
+        
         // Creating an insert query using SQL syntax and
         // storing it in a variable.
-        $sql_insert ="DELETE FROM animales WHERE id='$id'";
-
-          // The following code attempts to execute the SQL query
+        $sql_delete = "DELETE FROM animales WHERE id='".$_POST['Animal']."'";
+        mysqli_query($conexion,$sql_delete);
+        
+        // The following code attempts to execute the SQL query
           // if the query executes with no errors
           // a javascript alert message is displayed
           // which says the data is inserted successfully
-          mysqli_query($conexion,$sql_insert);
     }
 ?>
