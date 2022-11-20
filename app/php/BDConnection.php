cc194b246c06cbc72211af5e4fd150f2bb2b9db7
avasilica001 <113669366+avasilica001@users.noreply.github.com>
2022-11-20 19:19:37 +0100

    Delete app directory

--- app/php/BDConnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-    #Nos conectamos a la base de datos
-    $conexion = mysqli_connect("db", "sudo", "seguridad", "database");
-
-    if($conexion == false){
-        die("ERROR EN LA CONEXIÓN" . mysqli_connect_error());
-    }   
-?>
--- app/php/connectionCode.php
+++
@@ -1,5 +0,0 @@
-<?php
-# Nos conectamos a la base de datos
-$conexion = mysqli_connect("db", "admin", "test", "database");
-
-?>
